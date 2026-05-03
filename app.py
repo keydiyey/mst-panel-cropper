d75615fe4a2bd99cab429b58e57abe35c9ab63ab
kj <keydiyey@gmail.com>
2026-05-03 18:09:37 +0800

    added compression for faster uploading

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 import streamlit as st
 from utils import *
-import pathlib
+from PIL import Image
 import io
+
 
 st.set_page_config(page_title="Batch Image Cropper", layout="wide")
 
+
+def compress_image(file, quality=70, max_size=(1920, 1920)):
+    img = Image.open(file)
+    img.thumbnail(max_size)
+
+    buffer = io.BytesIO()
+    img.save(buffer, format="JPEG", quality=quality, optimize=True)
+    buffer.seek(0)
+    return buffer
 
 with st.sidebar:
     st.header("Upload Panels Here")
@@ -20,9 +30,9 @@ if uploaded_files:
     
     for uploaded_file in uploaded_files:
         if uploaded_file.name not in st.session_state.cropped_images:
-            uploaded_file.seek(0)
+            imagefile = compress_image(uploaded_file)
             
-            cropped_result = process(uploaded_file)
+            cropped_result = process(imagefile)
             
             st.session_state.cropped_images[uploaded_file.name] = {
                 "original": uploaded_file,
